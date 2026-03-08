f1bc33e67df9e8f669222d2e85c9fb59fad51822
SiddharthaGogoi <lezaroth3@gmail.com>
2026-03-08 19:59:37 +0530

    chore: add logs

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -19,6 +19,8 @@ export async function createGitHubApp() {
   const { data } = await githubApp.octokit.request('/app');
   console.log(`GitHub App authenticated as '${data.name}'`);
 
+  console.log('Data: ', data)
+
   githubApp.webhooks.on('installation.created', ({ payload }) => {
     const { installation, sender } = payload;
     const account = installation.account;
